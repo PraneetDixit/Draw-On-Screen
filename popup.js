4717708d1efef8748a9b965f0cd39a5851907d3b
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-03-06 20:10:32 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-//    var checkPageButton = document.getElementById('go');
-//    checkPageButton.addEventListener('click', function() {
-        chrome.tabs.executeScript(null ,{
-            file: "custom.js"
-        });
-//    });
+chrome.windows.getCurrent(function(wind) {
+    var maxWidth = window.screen.availWidth;
+    var maxHeight = window.screen.availHeight;
+    var updateInfo = {
+        left: 0,
+        top: 0,
+        width: maxWidth/2,
+        state: "normal",
+        height: maxHeight
+    };
+    chrome.windows.update(wind.id, updateInfo);
+    localStorage.setItem("baseDrawingWindowID", `${wind.id}`);
+});
+chrome.windows.create({
+    url: '/drawing.html',
+    type: 'normal',
+    width: window.screen.availWidth/2,
+    height: window.screen.availHeight,
+    focused: true,
+    left: window.screen.availWidth/2
 });
